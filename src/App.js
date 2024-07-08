3c423e63b48527ef2cb0a6243cb18b66417eeb21
Hassib <hassib-kevin.ezzedine@polymtl.ca>
2024-07-07 21:17:40 -0400

    ooga booga

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import { motion } from "framer-motion";
 
 function App() {
   return (
-    <div>
-      <motion.div animate={{ x: 100 }} />
-      <motion.h1
-        className="text-red-300 text-8xl text-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 4 }} // Adjust the duration as needed
-      >
-        Hello this is a test
-      </motion.h1>
-    </div>
+    <>
+      <header>
+        <link
+          href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+          rel="stylesheet"
+        />
+        <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+      </header>
+      <body>
+        <div>
+          <motion.div animate={{ x: 100 }} />
+          <motion.h1
+            className="text-red-300 text-8xl text-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 4 }} // Adjust the duration as needed
+          >
+            Hello this is a test
+          </motion.h1>
+        </div>
+      </body>
+    </>
   );
 }
 
