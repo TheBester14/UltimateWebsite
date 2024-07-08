3a92f2af177999e306e053836c7b4e2a1d1d0688
Hassib <hassib-kevin.ezzedine@polymtl.ca>
2024-07-07 21:17:40 -0400

    oogaa booga revolution

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+// kawaboonga
+function App() {
+  return (
+    <>
+      <header>
+        <link
+          href="https://fonts.googleapis.com/css?family=Press+Start+2P"
+          rel="stylesheet"
+        />
+        <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+      </header>
+
+      <div className="arcade-container">
+        <img src="" />
+        <></>
+      </div>
+    </>
+  );
+}
