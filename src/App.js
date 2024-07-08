47a0b4d2f2ad449eda2519f46a4db0ef8e6ab0de
TheBester14 <40973175+TheBester14@users.noreply.github.com>
2024-07-07 21:17:47 -0400

    test-a

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ function App() {
       <motion.div animate={{ x: 100 }} />
       <motion.h1
         className="text-red-300 text-8xl text-center"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 4 }} // Adjust the duration as needed
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.3,
+          ease: [0, 0.71, 0.2, 1.01],
+          scale: {
+            type: "spring",
+            damping: 5,
+            stiffness: 100,
+            restDelta: 0.001,
+          },
+        }}
       >
         Hello this is a test
       </motion.h1>
