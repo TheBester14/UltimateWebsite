a13458039413df9a91b59d595385f2cc840177aa
TheBester14 <40973175+TheBester14@users.noreply.github.com>
2024-07-11 22:26:38 -0400

    -Home // HKE

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
-
-const home = () => {
-  return <div>home</div>;
+import heartImg from "./heartImg.png";
+const Home = () => {
+  return (
+    <div className="bg-white text-5xl min-h-screen flex flex-col items-center justify-center text-center ">
+      <div className="flex space-x-2 mt-4">
+        <img
+          src={heartImg}
+          alt="heart-img"
+          className="object-scale-down h-36 w-36"
+        />
+        <img
+          src={heartImg}
+          alt="heart-img"
+          className="object-scale-down h-36 w-36"
+        />
+        <img
+          src={heartImg}
+          alt="heart-img"
+          className="object-scale-down h-36 w-36"
+        />
+      </div>
+      <h1 className="mb-12">Welcome to UltimateWebsite</h1>
+      <div className="text-2xl flex space-x-8">
+        <button type="button" className="nes-btn is-success">
+          Sign Up
+        </button>
+        <button type="button" className="nes-btn is-primary">
+          Log in
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default home;
+export default Home;
